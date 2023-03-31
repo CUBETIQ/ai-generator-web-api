c3da0b87c11b135e3952826a9f191e1e78c2f67f
Sambo Chea <sombochea@cubetiqs.com>
2023-03-31 08:53:52 +0700

    Add get emoji

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, jsonify, make_response, request, Response
+from marvin import ai_fn
+
+
+@ai_fn
+def get_emoji(text: str) -> str:
+    """
+    Returns an emoji that describes the provided text.
+    """
 
 
 def create_app():
@@ -8,5 +16,16 @@ def create_app():
     def index():
 
         return make_response(jsonify({'message': 'Hello World'}), 200)
+
+    @app.route('/generator', methods=['GET', 'POST'])
+    def generator():
+        fn = request.args.get('fn') or request.form.get('fn')
+        if fn:
+            # Check by fn name and invoke the function
+            if fn == 'emoji':
+                text = request.args.get('text') or request.form.get('text')
+                return make_response(jsonify({'emoji': get_emoji(text)}), 200)
+
+        return make_response(jsonify({'message': 'No function found'}), 404)
 
     return app
